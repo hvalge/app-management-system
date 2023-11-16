a60213438f520c83cedd4b601988c3c6ef9239e9
hevalg <hevalg@ttu.ee>
2023-11-16 18:11:41 +0200

    Update application search results

--- frontend/src/components/SearchApplications.vue
+++ frontend/src/components/SearchApplications.vue
@@ -1,20 +1,23 @@
 <template>
   <v-container>
-    <v-text-field v-model="searchQuery" label="Search Application services" @input="searchApplications" outlined></v-text-field>
+    <v-text-field v-model="searchQuery" label="Search Application Services" @input="searchApplications" outlined></v-text-field>
+
     <v-row v-if="applicationResults">
       <v-col cols="12" v-for="app in applicationResults" :key="app.appCode">
-        <v-card>
-          <!-- ... other card components ... -->
-          <v-list>
+        <v-card class="mb-4">
+          <v-card-title>{{ app.name }}</v-card-title>
+          <v-card-subtitle>{{ app.appGroup }}, {{ app.appType }}, Cost: {{ app.appCost }}</v-card-subtitle>
+          <v-card-text>{{ app.description }}</v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-list dense>
             <v-subheader>Services</v-subheader>
             <v-list-item-group>
-              <v-list-item v-for="service in app.appServices" :key="service.serviceCode">
+              <v-list-item v-for="service in app.appServices" :key="service.serviceCode" class="pl-4">
                 <v-list-item-content>
                   <v-list-item-title>{{ service.name }}</v-list-item-title>
-                  <!-- Display subtype only if it's not null -->
-                  <v-list-item-subtitle>
-                    Type: {{ service.type }}<template v-if="service.subType">, SubType: {{ service.subType }}</template>
-                  </v-list-item-subtitle>
+                  <v-list-item-subtitle>Type: {{ service.type }}<template v-if="service.subType">, SubType: {{ service.subType }}</template></v-list-item-subtitle>
                   <v-list-item-subtitle>{{ service.description }}</v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
