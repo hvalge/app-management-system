8c560bd355bc10cf65aa1b3fd701d3ac55301e37
hevalg <hevalg@ttu.ee>
2023-11-16 11:20:27 +0200

    Initial frontend setup

---
+++ frontend/src/services/apiService.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/api';
+
+export default {
+  createApplication(data: any) {
+    return axios.post(`${API_URL}/applications`, data);
+  },
+  searchApplications(name: string) {
+    return axios.get(`${API_URL}/applications`, { params: { name } });
+  },
+  createAppService(data: any) {
+    return axios.post(`${API_URL}/appservices`, data);
+  },
+  searchAppServices(name: string) {
+    return axios.get(`${API_URL}/appservices`, { params: { name } });
+  }
+};
